2381f8ff4d5c7eb6df2df3aeb18587f547eb3b26
jona2255 <jona2255@gmail.com>
2019-09-30 16:55:31 +0200

    Start

--- app/src/main/java/com/example/p4_widgets/MainActivity.java
+++ app/src/main/java/com/example/p4_widgets/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.p4_widgets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+
+    Switch aSwitch;
+    SharedPreferences prefs;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        prefs = getPreferences(MODE_PRIVATE);
+
+        aSwitch = findViewById(R.id.switch1);
+
+        aSwitch.setChecked(prefs.getBoolean("switch",false));
+
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                prefs.edit().putBoolean("switch", isChecked).apply();
+
+            }
+        });
+
+
     }
 }
