6848124cbc5a38442a7fa06969bf6b49ab3cb48a
jona2255 <jona2255@gmail.com>
2019-10-02 18:25:57 +0200

    Start

--- app/src/main/java/com/example/p4_widgets/MainActivity.java
+++ app/src/main/java/com/example/p4_widgets/MainActivity.java
@@ -4,14 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CheckedTextView;
 import android.widget.CompoundButton;
+import android.widget.RadioGroup;
 import android.widget.Switch;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
     Switch aSwitch;
     SharedPreferences prefs;
-
+    ToggleButton toggleButton;
+    CheckBox checkBox;
+    CheckedTextView checkedTextView;
+    RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +30,69 @@ public class MainActivity extends AppCompatActivity {
 
         aSwitch = findViewById(R.id.switch1);
 
-        aSwitch.setChecked(prefs.getBoolean("switch",false));
+        aSwitch.setChecked(prefs.getBoolean("Switch",false));
 
         aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
-                prefs.edit().putBoolean("switch", isChecked).apply();
+                prefs.edit().putBoolean("Switch", isChecked).apply();
 
             }
         });
+
+
+        toggleButton = findViewById(R.id.toggleButton);
+
+        toggleButton.setChecked(prefs.getBoolean("ToggleButton", false));
+
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                prefs.edit().putBoolean("ToogleButton", isChecked).apply();
+
+            }
+        });
+
+
+        checkBox = findViewById(R.id.checkBox);
+
+        checkBox.setChecked(prefs.getBoolean("CheckBox",false));
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                prefs.edit().putBoolean("CheckBox", isChecked).apply();
+            }
+        });
+
+        checkedTextView = findViewById(R.id.checkedTextView);
+
+        checkedTextView.setChecked(prefs.getBoolean("CheckedTextView", true));
+
+        checkedTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                checkedTextView.toggle();
+                prefs.edit().putBoolean("CheckedTextView", checkedTextView.isChecked()).apply();
+            }
+        });
+
+        radioGroup = findViewById(R.id.radioGroup);
+
+        radioGroup.check(prefs.getInt("RadioGroup", radioGroup.getCheckedRadioButtonId()));
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                prefs.edit().putInt("RadioGroup", checkedId).apply();
+            }
+        });
+
 
 
     }
